7dc43dd474d9f05c094403957bc590cd8cf5d0ad
Melinda Swartzfager <melinda@melindas-macbook-pro.local>
2018-02-27 14:49:12 -0800

    Added a skip button, working on code some more

--- Hybrid Meditation/Controllers/StartViewController.swift
+++ Hybrid Meditation/Controllers/StartViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class StartViewController: UIViewController {
     
+    var phaseOneTimer = Timer()
+    var phaseTwoTimer = Timer()
+    var phaseOneTimerOn = false
+    var phaseTwoTimerOn = false
+    
     @IBOutlet var minutesField: UITextField!
-    @IBOutlet var phaseOneTimer: UILabel!
-    @IBOutlet var phaseTwoTimer: UILabel!
+    @IBOutlet var phaseOneTimerLabel: UILabel!
+    @IBOutlet var phaseTwoTimerLabel: UILabel!
     
     @IBAction func startStopTimer(sender: UIButton) {
         if let minutesToMeditate = minutesField.text {
             let minutesAsInt = Int(minutesToMeditate)
             if minutesAsInt == nil {
-                let alertController = UIAlertController(title: "Minutes input error", message: "You must use an integer in the input box, eg: 30", preferredStyle: .alert)
+                let alertController = UIAlertController(title: "Minutes input error", message: "You must use an integer greater than 0 in the input box, eg: 30", preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                present(alertController, animated: true, completion: nil)
+            } else if minutesAsInt! <= 0 {
+                let alertController = UIAlertController(title: "Minutes input error", message: "You must use an integer greater than 0 in the input box, eg: 30", preferredStyle: .alert)
                 let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
                 alertController.addAction(defaultAction)
                 
@@ -31,6 +42,10 @@ class StartViewController: UIViewController {
     @IBAction func resetTimer(sender: UIButton) {
         
     }
+    @IBAction func skipPhase1(sender: UIButton) {
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
